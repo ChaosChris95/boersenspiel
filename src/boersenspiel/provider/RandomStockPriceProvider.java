1b061a93483a254d76a4ab2ae280e54b2048dfae
psychopeach89 <psychopeach89@b9553d2a-eb83-beb6-287f-36aefa1cb30e>
2013-05-02 19:43:52 +0000

    Was noch fehlt: - bringt bei dem Befehl GETALLSTOCK  NullPointerFehler

--- src/boersenspiel/provider/RandomStockPriceProvider.java
+++ src/boersenspiel/provider/RandomStockPriceProvider.java
@@ -35,7 +35,7 @@ public class RandomStockPriceProvider extends StockPriceProvider {  //TODO Where
             if (shareManagement.getShareByNumber(i) == null)
                 continue;
             erg = Math.round(Math.random() * (max - min + 1)+ min);
-            System.out.println(erg);           //TODO comment out later - just debugger
+            //System.out.println(erg);           //TODO comment out later - just debugger
             try{
                 shareManagement.getShareByNumber(i).increasePrice((long)erg);
             } catch (NegativeValueException e){
