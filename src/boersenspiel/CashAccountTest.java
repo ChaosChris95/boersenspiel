1774f5bbc934dd7d4c686a49270fa846d722193e
dnmet73 <dnmet73@gmail.com>
2013-05-04 09:32:57 +0000

    Failed commit: Default

--- src/boersenspiel/CashAccountTest.java
+++ src/boersenspiel/CashAccountTest.java
@@ -6,6 +6,49 @@ package boersenspiel;
  * Date: 04.05.13
  * Time: 11:09
  */
+
+import boersenspiel.account.CashAccount;
+import static org.junit.Assert.*;
+import org.junit.After;
+
 public class CashAccountTest {
 
+    private CashAccount cashAccount1 = new CashAccount(1000);
+    private CashAccount cashAccount2 = new CashAccount(2000);
+
+    //@Before
+    public void setUp() {}
+
+    //@After
+    public void tearDown() {}
+
+    //@BeforeClass
+    public static void setUpBeforeClass() {}
+
+    //@AfterClass
+    public static void tearDownAfterClass() {}
+
+    //@Test
+    public void testGetValue() {
+        //assertEquals("1000",new CashAccount(1000), cashAccount1.equals(cashAccount2));
+    }
+
+    //@Test
+    public void testSubtract() {
+        //assertEquals("30-20=10",new Money(10), money1.subtract(money2));
+    }
+
+    //@Test(expected=NotEnoughMoneyException)
+    public void testSubCash() {
+
+    }
+
+    //@Test
+    public void testToString() {
+
+    }
+
+    public void testAddCash(){
+
+    }
 }
